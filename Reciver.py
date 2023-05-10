cde1734b782c2f2996bd03460fbb905381da714f
Joshua gordon <jodogo9897@gmail.com>
2023-05-10 20:15:18 +0300

    modified:   Reciver.py 	modified:   Sender.py 	modified:   random.txt 	modified:   received_file.txt

--- Reciver.py
+++ Reciver.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import os
 
 class Receiver:
     def __init__(self, filename, host, port, id_1, id_2):
@@ -12,87 +13,126 @@ class Receiver:
         #ids for XOR auth operation
         self.xor = bin(int(id_1)^int(id_2))[2:]
         self.auth = str(self.xor).encode()
+        self.conn = None
+        self.addr = None
+        self.filesize = 0
+        self.s = None
+        self.max = 0
+        self.ended = 0
 
-    def run(self):
+    def run(self, flag):
         #open file for writing
         with open(self.filename, 'wb') as f:
             #initilize socket
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                #bind & listen
+            if flag == 0:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s = s
+            else:
+                s = self.s
+            #bind & listen
+            if flag == 0:
                 s.bind((self.host, self.port))
                 s.listen()
                 conn, addr = s.accept()
-                with conn:
-                    print('Connected by', addr)
-                    #start timer for fist half sent
-                    start_time = time.time()
-                    #recive file size & make first and second half sizes
+                self.conn = conn
+                self.addr = addr
+            else:
+                conn = self.conn
+                addr = self.addr
+            with conn:
+                print('Connected by', addr)
+                #recive file size & make first and second half sizes
+                if flag == 0:
                     file_size_str = conn.recv(self.chunk_size)
+                    conn.sendall(self.auth)
                     file_size = int(file_size_str.decode())
-                    first_half_size = file_size//2
-                    total_recived = 0
-                    #server loop
-                    while True:
-                        #recive data and write to file
-                        data = conn.recv(self.chunk_size)
-                        f.write(data)
-                        #total recived increased
-                        total_recived+=len(data)
-                        #if finshed first half
-                        if total_recived == first_half_size:
-                            #send auth
-                            conn.sendall(self.auth)
-                            #end timer
-                            end_time_first_half = time.time()
-                            #total first half time
-                            first_half_time = end_time_first_half - start_time
-                            #appending data to log
-                            self.log.append(first_half_time)
-                        #if second half finished
-                        if total_recived == file_size:
-                            #send auth
-                            conn.sendall(self.auth)
-                            #end timer
-                            end_second_half_time = time.time()
-                            second_half_time = end_second_half_time - end_time_first_half
-                            #append second half total time
-                            self.log.append(second_half_time)
-                            print('file recived')
-                            #response for if to send again or end
-                            response = conn.recv(self.chunk_size)
-                            if response.decode() == 'RESEND':
-                                
-                                conn.sendall(b'OK')
-                                s.close()
-                                #rerun
-                                self.run()
-                            elif response.decode() == 'BYE':
-                                end_time = time.time()
-                                conn.sendall(b'OK')
-                                #for statatistics
-                                avg_first = 0
-                                avg_second = 0
-                                count = 0
-                                total = 0
-                                #run throgh log and make avgs, totals
-                                for i in range(len(self.log)):
-                                    if i % 2 == 0:
-                                        print(f'first half iteration {int((i+1)/2)+1} sent successfully in {self.log[i]} seconds.')
-                                        avg_first+=self.log[i]
-                                    else:
-                                        print(f'second half iteration {int(i/2)+1} sent successfully in {self.log[i]} seconds.')
-                                        avg_second+=self.log[i]
+                    self.filesize = file_size
+                else:
+                    file_size = self.filesize
 
-                                    count+=1
-                                    if i == 0:
-                                        total+=self.log[i]
-                                    else:
-                                        total+= self.log[i-1]+self.log[i]
-                                print('*************************************************************************')
-                                print(f'first half avg: sent successfully in {avg_first/(count/2)} seconds.')
-                                print(f'second half avg: sent successfully in {avg_second/(count/2)} seconds.')
-                                print(f'total avg: sent successfully in {total/(count)} seconds.')
+                first_half_size = file_size//2
+                total_recived = 0
+            
+                #server loop
+                #start timer for fist half sent
+                start_time = time.time()
+                while True:
+                    #recive data and write to file
+                    data = conn.recv(self.chunk_size)
+                    f.write(data)
+                    #total recived increased
+                    total_recived+=len(data)
+                    #if finshed first half
+                    if total_recived == first_half_size:
+
+                        size = os.path.getsize(self.filename)
+                        print(f'first half recived - size of : {size}')
+                        #send auth
+                        conn.sendall(self.auth)
+                        #end timer
+                        end_time_first_half = time.time()
+                        #total first half time
+                        first_half_time = end_time_first_half - start_time
+                        #appending data to log
+                        self.log.append(first_half_time)
+                    #if second half finished
+                    if total_recived == file_size:
+                        size = os.path.getsize(self.filename)
+                        print(f'seoond half recived - size of : {size}')
+                        #send auth
+                        conn.sendall(self.auth)
+                        #end timer
+                        end_second_half_time = time.time()
+                        second_half_time = end_second_half_time - end_time_first_half
+                        #append second half total time
+                        self.log.append(second_half_time)
+                        print('file recived')
+                        #response for if to send again or end
+                        response = conn.recv(self.chunk_size)
+                        if response.decode() == 'RESEND':
+                            
+                            conn.sendall(b'OK')
+                            
+                            #rerun
+                            self.run(1)
+                            if self.ended == 1:
                                 break
+                        elif response.decode() == 'BYE':
+
+                            conn.sendall(b'OK')
+                            conn.close()
+                            self.max = max(self.log)
+                            time.sleep(2*self.max)
+                            s.close()
+                            self.ended = 1
+                            #for statatistics
+                            avg_first = 0
+                            avg_second = 0
+                            count = 0
+                            total = 0
+                            #run throgh log and make avgs, totals
+                            print(' ')
+                            print('Statistics:')
+                            print('*************************************************************************')
+                            for i in range(len(self.log)):
+                                if i % 2 == 0:
+                                    print(f'first half iteration {int((i+1)/2)+1} sent successfully in {self.log[i]} seconds.')
+                                    avg_first+=self.log[i]
+                                else:
+                                    print(f'second half iteration {int(i/2)+1} sent successfully in {self.log[i]} seconds.')
+                                    avg_second+=self.log[i]
+
+                                count+=1
+                                if i == 0:
+                                    total+=self.log[i]
+                                else:
+                                    total+= self.log[i-1]+self.log[i]
+                            print('*************************************************************************')
+                            print(f'first half avg: sent successfully in {avg_first/(count/2)} seconds.')
+                            print(f'second half avg: sent successfully in {avg_second/(count/2)} seconds.')
+                            print(f'total avg: sent successfully in {total/(count)} seconds.')
+                            print('*************************************************************************')
+                            break
 
 def main():
 
@@ -101,7 +141,7 @@ def main():
     port = 9999 # port 9999 free
 
     r1 = Receiver(filename, host, port, '332307073', '334307083')
-    r1.run()
+    r1.run(0)
 
 
 if __name__ == "__main__":
--- Sender.py
+++ Sender.py
@@ -6,64 +6,82 @@ import os
 
 
 class Sender:
-    def __init__(self, host, port, filename):
+    def __init__(self, host, port, filename, id_1, id_2):
         self.host = host
         self.port = port
         self.filename = filename
         #random size between 3MB - 6MB file size of chars
         mb_size = random.randint(3,6)*1024*1024
+        self.filesize = mb_size
         self.create_random_file(mb_size)
-        self.packet_loss = 0
+        self.s = None
+        self.xor = bin(int(id_1)^int(id_2))[2:]
+        self.expected_auth = str(self.xor).encode()
+        
 
     def create_random_file(self, mb_size):
         with open("{}".format(self.filename), "w") as f:
-            for i in range(mb_size):
+            count = 1
+            while os.path.getsize(self.filename) < mb_size:
                 f.write(random.choice(string.ascii_letters))
+                if(count%50 == 0):
+                    count = 0
+                    f.write('\n')
+                count+=1
+                
+                
 
-    def send_file(self):
-        
-        # read the file
-        filesize = os.path.getsize(self.filename)
-        first_half = int(filesize // 2)
+    def send_file(self, flag):
+        first_half = self.filesize // 2 
+        # 1.	At first, you’ll read the file you’ve created.
         with open(self.filename, 'rb') as f:
             data = f.read()
             first_data = data[:first_half]
             second_data = data[first_half:]
 
-        # create the TCP connection
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        print(f"Connected to {self.host}:{self.port}")
+        # 2.	Create a TCP Connection between the sender and receiver.
+        if flag == 0:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_TCP, socket.TCP_CONGESTION, b'reno')
+            s.connect((self.host, self.port))
+            print(f"Connected to {self.host}:{self.port}")
+            self.s = s
+        else:
+            s = self.s
         
-        filesize = os.path.getsize(self.filename)
-        first_half = int(filesize // 2)
-        #open and read file to data
-        with open(self.filename, 'rb') as f:
-            data = f.read()
-            first_data = data[:first_half]
-            second_data = data[first_half:]
         #send length of file
-        start_time = time.time()
-        s.sendall(str(filesize).encode())
+        if flag == 0:
+            
+            s.sendall(str(self.filesize).encode())
+            auth = s.recv(1024)
+            
+            if auth == self.expected_auth:
+                print('Recived ack on file size')
         # send first half of the file
         s.sendall(first_data)
-        end_time = time.time()
-        print(f"Sent first half of file in {end_time - start_time} seconds.")
+        print("Sent first half of file:")
 
         # receive authentication for first half
         auth = s.recv(1024)
-        print(f"Authentication received for first half: {auth}")
+        if auth == self.expected_auth:
+            print(f"Authentication received for first half: {auth}")
 
+        #change CC algorithm
+        s.setsockopt(socket.SOL_TCP, socket.TCP_CONGESTION, b'cubic')
+        print("CC algorithm changed to cubic:")
         # send second half of the file
-        start_time = time.time()
         s.sendall(second_data)
-        end_time = time.time()
-        print(f"Sent second half of file in {end_time - start_time} seconds.")
+        print("Sent second half of file:")
+        
 
         # receive authentication for second half
         auth = s.recv(1024)
-        print(f"Authentication received for second half: {auth}")
-
+        if auth == self.expected_auth:
+            print(f"Authentication received for second half: {auth}")
+        print("Sent second half of file:")
+        size = os.path.getsize(self.filename)
+        print(' ')
+        print(f'total file sent - size of : {size}')
         # ask user if they want to send the file again
         send_again = input("Do you want to send the file again? (y/n) ")
         if send_again.lower() == 'y':
@@ -72,12 +90,10 @@ class Sender:
             print("Notified receiver to send again.")
             auth = s.recv(1024)
             # change back CC algorithm
-            #s.setsockopt(socket.SOL_TCP, socket.TCP_CONGESTION, b"cubic")
-            #print("CC algorithm changed back to cubic.")
-
-            # close the connection and send file again
-            s.close()
-            self.send_file()
+            self.s.setsockopt(socket.SOL_TCP, socket.TCP_CONGESTION, b'reno')
+            print("CC algorithm changed back to reno.")
+            print(' ')
+            self.send_file(1)
         else:
             # say bye to the receiver and close the connection
             s.sendall(b"BYE")
@@ -87,5 +103,5 @@ class Sender:
 
 
 if __name__ == "__main__":
-    sender = Sender('127.0.0.1', 9999, 'random.txt')
-    sender.send_file()
+    sender = Sender('127.0.0.1', 9999, 'random.txt','332307073', '334307083')
+    sender.send_file(0)
